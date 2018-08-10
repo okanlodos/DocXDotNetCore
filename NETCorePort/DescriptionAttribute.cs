481e2b04b59b0df8136d4db8a3cbf75ff5e99a65
Unknown <okanlodos@gmail.com>
2018-08-11 01:52:41 +0300

    added project

---
+++ NETCorePort/DescriptionAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+#if NETSTANDARD1_6
+namespace Novacode.NETCorePort
+{
+
+    public class DescriptionAttribute:Attribute
+    {
+        public DescriptionAttribute(string description)
+        {
+            Description = description;
+        }
+
+        public string Description { get; }
+    }
+}
+#endif
